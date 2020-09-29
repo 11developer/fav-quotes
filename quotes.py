3617a5b8430d6e95123f6a85bcec84fb789b61b5
11developer <67899620+11developer@users.noreply.github.com>
2020-09-29 09:10:21 +0300

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:password@localhost/quotes'
 #Course-db
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://kutylncrmosvvrvif:56786171441e668e08b0b5bda470a3673e6e879f94466b5e7865eb9ad18268c1f807c3@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/d7aofkhqpvufo2mq'
-#app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://tzwmlmpkdkliuh:f848bd744b405a3fbe8eafe3f1a24c9e5a7f1c14e2aa3bee67e889dc900c26af@ec2-54-243-67-199.compute-1.amazonaws.com:5432/dbvkie13pqfn1l'
+#app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://kutylncrmosvvrvif:56786171441e668e08b0b5bda470a3673e6e879f94466b5e7865eb9ad18268c1f807c3@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/d7aofkhqpvufo2mq'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://tzwmlmpkdkliuh:f848bd744b405a3fbe8eafe3f1a24c9e5a7f1c14e2aa3bee67e889dc900c26af@ec2-54-243-67-199.compute-1.amazonaws.com:5432/dbvkie13pqfn1l'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
